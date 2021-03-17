61dd35757adb7a59743d65fdf95dd4db9de0d252
Felipe Santos <felipecassiors@gmail.com>
2021-03-17 17:08:35 -0300

    feat: generate semver tags (#30)
    - SemVer tags are :major, :major.minor and :major.minor.patch.
    - They will be generated by default if all the criterions match:
      - version is a valid semver
      - `--latest` is enabled
    - New option: --tag-semver, if set, it will enforce the generation of
    semver tags, and will cause docker-meta to fail if a non-valid semver is
    supplied as `version`.
    
    BREAKING CHANGE: Now semver tags are generated by default if a valid
    semver is detected as `version` (you can disable this behavior with the
    new option `--no-tag-semver`).
    
    BREAKING CHANGE: Now the version will be normalized if it is a valid
    semver, for example: `v1.0.0` becomes `1.0.0`.

--- test/index.test.ts
+++ test/index.test.ts
@@ -24,7 +24,9 @@ describe("docker-meta", () => {
     dir = tmp.dirSync({ unsafeCleanup: true });
     shell.cd(dir.name);
     execa.commandSync("git init");
-    execa.commandSync("git commit --no-edit --allow-empty --allow-empty-message");
+    execa.commandSync(
+      "git commit --no-edit --allow-empty --allow-empty-message"
+    );
     shell.cp("-f", configPath, "docker-meta.config.js");
   });
 
@@ -68,7 +70,7 @@ describe("docker-meta", () => {
       }`);
   });
   it("generates correctly for non change-request", async () => {
-    process.env.VERSION = "1.1.1";
+    process.env.VERSION = "1.2.3";
     process.env.LATEST = "true";
     process.env.BRANCH = "develop";
     process.env.CHANGE_REQUEST = "false";
@@ -83,8 +85,12 @@ describe("docker-meta", () => {
             "target": {
               "docker-meta": {
                 "tags": [
-                  "felipecrs/docker-meta:1.1.1",
-                  "ghcr.io/felipecrs/docker-meta:1.1.1",
+                  "felipecrs/docker-meta:1.2.3",
+                  "ghcr.io/felipecrs/docker-meta:1.2.3",
+                  "felipecrs/docker-meta:1",
+                  "ghcr.io/felipecrs/docker-meta:1",
+                  "felipecrs/docker-meta:1.2",
+                  "ghcr.io/felipecrs/docker-meta:1.2",
                   "felipecrs/docker-meta:develop",
                   "ghcr.io/felipecrs/docker-meta:develop",
                   "felipecrs/docker-meta:latest",
@@ -93,15 +99,146 @@ describe("docker-meta", () => {
                 "labels": {
                   "org.label-schema.vsc-ref": "81a88f4",
                   "org.label-schema.build-date": "docker-meta",
-                  "org.label-schema.version": "1.1.1",
+                  "org.label-schema.version": "1.2.3",
                   "org.label-schema.schema-version": "1.0.0-rc1"
                 },
                 "args": {
-                  "VERSION": "1.1.1",
+                  "VERSION": "1.2.3",
                   "BRANCH": "develop"
                 }
               }
             }
           }`);
+  });
+
+  it("disables tag-semver properly", async () => {
+    process.env.VERSION = "1.2.3";
+    process.env.LATEST = "true";
+    process.env.BRANCH = "develop";
+    process.env.CHANGE_REQUEST = "false";
+    process.env.GIT_COMMIT = "81a88f4";
+
+    const result = execa.commandSync(`${bin} -o dm.json --no-tag-semver`);
+    expect(result.stdout).toBe("");
+    expect(result.exitCode).toBe(0);
+
+    expect(fs.readFileSync("dm.json").toString()).toEqual(stripIndent`
+          {
+            "target": {
+              "docker-meta": {
+                "tags": [
+                  "felipecrs/docker-meta:1.2.3",
+                  "ghcr.io/felipecrs/docker-meta:1.2.3",
+                  "felipecrs/docker-meta:develop",
+                  "ghcr.io/felipecrs/docker-meta:develop",
+                  "felipecrs/docker-meta:latest",
+                  "ghcr.io/felipecrs/docker-meta:latest"
+                ],
+                "labels": {
+                  "org.label-schema.vsc-ref": "81a88f4",
+                  "org.label-schema.build-date": "docker-meta",
+                  "org.label-schema.version": "1.2.3",
+                  "org.label-schema.schema-version": "1.0.0-rc1"
+                },
+                "args": {
+                  "VERSION": "1.2.3",
+                  "BRANCH": "develop"
+                }
+              }
+            }
+          }`);
+  });
+
+  it("disables tag-semver if version is invalid and tag-semver is auto", async () => {
+    process.env.VERSION = "invalid1.2.3";
+    process.env.LATEST = "true";
+    process.env.BRANCH = "develop";
+    process.env.CHANGE_REQUEST = "false";
+    process.env.GIT_COMMIT = "81a88f4";
+
+    const result = execa.commandSync(`${bin} -o dm.json`);
+    expect(result.stdout).toBe("");
+    expect(result.exitCode).toBe(0);
+
+    expect(fs.readFileSync("dm.json").toString()).toEqual(stripIndent`
+          {
+            "target": {
+              "docker-meta": {
+                "tags": [
+                  "felipecrs/docker-meta:invalid1.2.3",
+                  "ghcr.io/felipecrs/docker-meta:invalid1.2.3",
+                  "felipecrs/docker-meta:develop",
+                  "ghcr.io/felipecrs/docker-meta:develop",
+                  "felipecrs/docker-meta:latest",
+                  "ghcr.io/felipecrs/docker-meta:latest"
+                ],
+                "labels": {
+                  "org.label-schema.vsc-ref": "81a88f4",
+                  "org.label-schema.build-date": "docker-meta",
+                  "org.label-schema.version": "invalid1.2.3",
+                  "org.label-schema.schema-version": "1.0.0-rc1"
+                },
+                "args": {
+                  "VERSION": "invalid1.2.3",
+                  "BRANCH": "develop"
+                }
+              }
+            }
+          }`);
+  });
+
+  it("normalizes the version", async () => {
+    process.env.VERSION = "v1.2.3";
+    process.env.LATEST = "true";
+    process.env.BRANCH = "develop";
+    process.env.CHANGE_REQUEST = "false";
+    process.env.GIT_COMMIT = "81a88f4";
+
+    const result = execa.commandSync(`${bin} -o dm.json`);
+    expect(result.stdout).toBe("");
+    expect(result.exitCode).toBe(0);
+
+    expect(fs.readFileSync("dm.json").toString()).toEqual(stripIndent`
+          {
+            "target": {
+              "docker-meta": {
+                "tags": [
+                  "felipecrs/docker-meta:1.2.3",
+                  "ghcr.io/felipecrs/docker-meta:1.2.3",
+                  "felipecrs/docker-meta:1",
+                  "ghcr.io/felipecrs/docker-meta:1",
+                  "felipecrs/docker-meta:1.2",
+                  "ghcr.io/felipecrs/docker-meta:1.2",
+                  "felipecrs/docker-meta:develop",
+                  "ghcr.io/felipecrs/docker-meta:develop",
+                  "felipecrs/docker-meta:latest",
+                  "ghcr.io/felipecrs/docker-meta:latest"
+                ],
+                "labels": {
+                  "org.label-schema.vsc-ref": "81a88f4",
+                  "org.label-schema.build-date": "docker-meta",
+                  "org.label-schema.version": "1.2.3",
+                  "org.label-schema.schema-version": "1.0.0-rc1"
+                },
+                "args": {
+                  "VERSION": "1.2.3",
+                  "BRANCH": "develop"
+                }
+              }
+            }
+          }`);
+  });
+
+  it("refuses to work if tag-semver is enabled and version is not valid", async () => {
+    process.env.VERSION = "invalid1.2.3";
+    process.env.LATEST = "true";
+    process.env.BRANCH = "develop";
+    process.env.CHANGE_REQUEST = "false";
+    process.env.GIT_COMMIT = "81a88f4";
+
+    const result = execa.commandSync(`${bin} -o dm.json --tag-semver`, {
+      reject: false,
+    });
+    expect(result.exitCode).toBe(2);
   });
 });
