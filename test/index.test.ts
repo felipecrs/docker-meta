1cb898fd46d5c5851fb3242810f991b65185fae6
Felipe Santos <felipecassiors@gmail.com>
2023-09-20 15:45:34 -0300

    feat: add --tag-git-sha to generate tags like `:sha-<git-sha>`

--- test/index.test.ts
+++ test/index.test.ts
@@ -266,6 +266,50 @@ describe("docker-meta", () => {
           }`);
   });
 
+  it("tags with git-sha properly", async () => {
+    process.env.VERSION = "1.2.3";
+    process.env.LATEST = "true";
+    process.env.BRANCH = "develop";
+    process.env.CHANGE_REQUEST = "false";
+    process.env.GIT_COMMIT = "81a88f456";
+
+    const result = execa.commandSync(`${bin} -o dm.json --tag-git-sha`);
+    expect(result.stdout).toBe("");
+    expect(result.exitCode).toBe(0);
+
+    expect(fs.readFileSync("dm.json").toString()).toEqual(stripIndent`
+          {
+            "target": {
+              "docker-meta": {
+                "tags": [
+                  "felipecrs/docker-meta:sha-81a88f4",
+                  "ghcr.io/felipecrs/docker-meta:sha-81a88f4",
+                  "felipecrs/docker-meta:1.2.3",
+                  "ghcr.io/felipecrs/docker-meta:1.2.3",
+                  "felipecrs/docker-meta:1",
+                  "ghcr.io/felipecrs/docker-meta:1",
+                  "felipecrs/docker-meta:1.2",
+                  "ghcr.io/felipecrs/docker-meta:1.2",
+                  "felipecrs/docker-meta:develop",
+                  "ghcr.io/felipecrs/docker-meta:develop",
+                  "felipecrs/docker-meta:latest",
+                  "ghcr.io/felipecrs/docker-meta:latest"
+                ],
+                "labels": {
+                  "org.label-schema.vsc-ref": "81a88f4",
+                  "org.label-schema.build-date": "docker-meta",
+                  "org.label-schema.version": "1.2.3",
+                  "org.label-schema.schema-version": "1.0.0-rc1"
+                },
+                "args": {
+                  "VERSION": "1.2.3",
+                  "BRANCH": "develop"
+                }
+              }
+            }
+          }`);
+  });
+
   it("refuses to work if tag-semver is enabled and version is not valid", async () => {
     process.env.VERSION = "invalid1.2.3";
     process.env.LATEST = "true";
